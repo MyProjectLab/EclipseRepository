c3c16f12b0dd67b02a8924ef94fcad5b857fdfd2
Nitin <nitinshuklavkit@gmail.com>
2023-09-01 03:24:41 +0530

    2nd commit

---
+++ src/test/java/GIT_Test/Appium.java
@@ -0,0 +1,10 @@
+package GIT_Test;
+
+public class Appium {
+	
+	public void addCountry()
+	{
+		System.out.println("india added");
+	}
+
+}
---
+++ src/test/java/GIT_Test/Mobile.java
@@ -0,0 +1,5 @@
+package GIT_Test;
+
+public class Mobile {
+
+}
